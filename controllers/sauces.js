6d9d1cc5d65a1cdaa69cda61db54ff2d5f2f2b31
christian <delormechrist@gmail.com>
2020-09-03 13:51:29 +0200

    multer

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,11 +1,16 @@
- const { syncBuiltinESMExports } = require("module");
+// const { syncBuiltinESMExports } = require("module");
+// const { json } = require("body-parser");
 
 const Sauces = ('../models/Sauces');
+const fs = require('fs');
 
 exports.createSauces = (req, res, next) => {
     // delete req.body._id;
+    //delate sauceObject._id;
+    const saucesObject = json.parse(req.body.sauces);
     const sauces = new sauces({
-        ...req.body
+        ...saucesObject,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${res.file.filename}`
     });
     thing.save()
     .then(() => res.status(201).json({message: 'Sauces enregistrées'}))
@@ -19,15 +24,29 @@ exports.sendSauces = (req, res, next) => {
   };
 
 exports.modifySauces = (req, res, next) => {
-    Sauces.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+   const saucesObject = req.file ?
+   {
+     ...JSON.parse(req.body.sauces),
+     imageUrl: `${req.protocol}://${req.get('host')}/images/${res.file.filename}`
+
+   } : { ...req.body };
+  Sauces.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'mise a jour des sauces !'}))
       .catch(error => res.status(400).json({ error }));
   };
 
 exports.deleteSauces = (req, res, next) => {
-    sauces.delete({_id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
+    sauces.findOne({ _id: req.params.id})
+    .then(sauces => {
+      const filename = sauces.imageUrl.split('/images/')[2];
+      fs.unlink(`images/${filename}`, () =>{
+        sauces.deleteOne({_id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
+      } )
+    })
+    .catch( error => error.status(500).json({ error}));
+    
 };
 
 exports.findOneSauce = (req, res, next) => {
--- routes/sauces.js
+++ routes/sauces.js
@@ -3,16 +3,16 @@ const router = express.Router();
 
 const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
-
+const multer = require ('../middleware/multer-config');
 
 // router.post('/', (req, res, next) => {
 //   console.log(req.body);
 //   res.status(201).json({message: 'une sauce'})
 // });
 
-router.post('/', auth, saucesCtrl.createSauces);   //[Capture et enregistre l'image, analyse la sauce, enregistre ds la base de donées]
+router.post('/', auth, multer, saucesCtrl.createSauces);   //[Capture et enregistre l'image, analyse la sauce, enregistre ds la base de donées]
 router.get('/', auth, saucesCtrl.sendSauces);   //[renvoie le tableau de toutes les sauces ds la bases de données]
-router.put('/:id', auth, saucesCtrl.modifySauces);   //[Met a jour les sauces ]
+router.put('/:id', auth, multer, saucesCtrl.modifySauces);   //[Met a jour les sauces ]
 router.delete('/:id', auth, saucesCtrl.deleteSauces);   //[Supprime la sauce]
 router.get('/:id', auth, saucesCtrl.findOneSauce);  //[Renvoie la sauce avec l'ID fourni]
 
